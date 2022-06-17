7da5da882a6ef6c6edd32f036834a099ed543119
MarinBalashkov <Mkbalashkov@students.softuni.bg>
2022-06-17 23:14:12 +0300

    add page.js function

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@ import { render } from '../node_modules/lit-html/lit-html.js';
 import { logout as apiLogout } from './api/data.js';
 
 
-// import { homePage } from './views/home.js';
+import { homePage } from './views/home.js';
+import { registerPage } from './views/register.js';
+import { loginPage } from './views/login.js';
 // import { catalogPage } from './views/catalog.js';
 // import { normalCatalogPage } from './views/normalCatalog.js';
 // import { myListingPage } from './views/myListings.js';
 // import { createPage } from './views/create.js';
-// import { loginPage } from './views/login.js';
-// import { registerPage } from './views/register.js';
 // import { editPage } from './views/edit.js';
 // import { detailsPage } from './views/details.js';
 // import { searchByYearPage } from './views/searchByYear.js';
@@ -23,42 +23,44 @@ document.getElementById('logoutBtn').addEventListener('click', logout);
 setUserNav();
 
 
-// page('/', decorateContext, homePage);
-// page('/home', decorateContext, homePage);
+page('/register', decorateContext, registerPage);
+page('/login', decorateContext, loginPage);
+page('/', decorateContext, homePage);
+page('/home', decorateContext, homePage);
 // page('/catalog', decorateContext, normalCatalogPage);
 // page('/myListings', decorateContext, myListingPage);
 // page('/details/:id', decorateContext, detailsPage);
 // page('/edit/:id', decorateContext, editPage);
 // page('/create', decorateContext, createPage);
-// page('/register', decorateContext, registerPage);
-// page('/login', decorateContext, loginPage);
 // page('/searchByYear', decorateContext, searchByYearPage);
 
 
-// page.start();
+page.start();
 
 
-// function decorateContext(ctx, next) {
-//     ctx.setUserNav = setUserNav;
-//     ctx.user = getUserData();
-//     ctx.render = (content) => render(content, main);
+function decorateContext(ctx, next) {
+    ctx.setUserNav = setUserNav;
+    ctx.userId = sessionStorage.getItem('userId');
+    ctx.render = (content) => render(content, main);
 
-//     next();
-// }
+    next();
+}
 
 function setUserNav() {
-    const token = sessionStorage.getItem('authToken');
-    if (token) {
-        [...document.querySelectorAll('.profile')].map(x => x.style.display = 'block inline');
+    console.log('In userNav')
+    const userId = sessionStorage.getItem('userId');
+    if (userId) {
+        [...document.querySelectorAll('.profile')].map(x => x.style.display = 'inline');
         [...document.querySelectorAll('.guest')].map(x => x.style.display = 'none');
     } else {
         [...document.querySelectorAll('.profile')].map(x => x.style.display = 'none');
-        [...document.querySelectorAll('.guest')].map(x => x.style.display = 'block inline');
+        [...document.querySelectorAll('.guest')].map(x => x.style.display = 'inline');
     }
 }
 
-function logout(event) {
-    apiLogout();
+async function logout(event) {
+    event.preventDefault();
+    await apiLogout();
     setUserNav();
-    page.redirect('/');
+    page.redirect('/home');
 }
